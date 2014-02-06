e128e3405315ef5f5fba79677dba006f4d6ae67d
LethaK Maas <github@souverain.org>
2014-02-06 18:34:17 +0100

    Update ltkOrgSyphon.php

--- ltkOrgSyphon.php
+++ ltkOrgSyphon.php
@@ -23,55 +23,76 @@ $selectorList = array(
 );
 
 $crawler = array(
-	'currentPage'=>1,
+	'currentPage'=>0,
 	'orgPerPage'=>255,
 );
-
-//$MARKUP = file_get_contents('https://robertsspaceindustries.com/community/orgs/listing?sort=size_desc&search=&language[]=fr&pagesize='.$crawler['orgPerPage'].'&page='.$crawler['currentPage'].'&');
-//$MARKUP = file_get_contents('sample.html');
-
-$doc = phpQuery::newDocument($MARKUP);
-phpQuery::selectDocument($doc);
-$containers = phpQuery::pq($selectorList['org']);
+$crawler_notFinished = true;
 
 $ORGANIZATIONS = array(
+	'webQueryCount' => 0,
+	'crawled' => array(),
 	'count' => 0,
+	'listCount' => 0,
 	'list' => array(),
 );
-$ORGANIZATIONS['count'] = ((int)trim(''.reset(phpQuery::pq($selectorList['org/count'])->getStrings())));
 
-foreach ($containers as $container)
+while($crawler_notFinished)
 {
-	$item = phpQuery::pq($container);
-	$ORG = array(
-		'spectrumId' => trim(''.reset($item[$selectorList['org/spectrumid']]->getStrings())),
-		'name' => trim(''.reset($item[$selectorList['org/name']]->getStrings())),
-		'logo' => trim(''.$item[$selectorList['org/logo']]->attr('src')),
-		'isDefaultLogo' => 0,
-	);
+	$ORGANIZATIONS['webQueryCount']++;
+		$crawler['currentPage']++;
+	
+	$url = 'https://robertsspaceindustries.com/community/orgs/listing?sort=size_desc&search=&language[]=fr&pagesize='.$crawler['orgPerPage'].'&page='.$crawler['currentPage'].'&';
+	$MARKUP = file_get_contents($url);
+	//$MARKUP = file_get_contents('sample.html');
+	$ORGANIZATIONS['crawled'][] = $url;
+	
 
-	foreach ($item[$selectorList['org/infoitem']] as $infoitem)
+	$doc = phpQuery::newDocument($MARKUP);
+	phpQuery::selectDocument($doc);
+	$containers = phpQuery::pq($selectorList['org']);
+
+
+	if($ORGANIZATIONS['count'] === 0)
+		$ORGANIZATIONS['count'] = ((int)trim(''.reset(phpQuery::pq($selectorList['org/count'])->getStrings())));
+
+	foreach ($containers as $container)
 	{
-		$infoitem_ = phpQuery::pq($infoitem);
-		$label = '_'.substr(trim(''.$infoitem_[$selectorList['org/infoitem/label']]->html()), 0, -1);
-		$ORG[$label] = substr(trim(''.$infoitem_[$selectorList['org/infoitem/value']]->html()), 0);
+		$item = phpQuery::pq($container);
+		$ORG = array(
+			'spectrumId' => trim(''.reset($item[$selectorList['org/spectrumid']]->getStrings())),
+			'name' => trim(''.reset($item[$selectorList['org/name']]->getStrings())),
+			'logo' => trim(''.$item[$selectorList['org/logo']]->attr('src')),
+			'isDefaultLogo' => 0,
+		);
+
+		foreach ($item[$selectorList['org/infoitem']] as $infoitem)
+		{
+			$infoitem_ = phpQuery::pq($infoitem);
+			$label = '_'.substr(trim(''.$infoitem_[$selectorList['org/infoitem/label']]->html()), 0, -1);
+			$ORG[$label] = substr(trim(''.$infoitem_[$selectorList['org/infoitem/value']]->html()), 0);
+		}
+
+
+		$matches = array();
+		preg_match("/organization\/defaults\/logo/", $ORG['logo'], $matches);
+		if(count($matches)>0)
+			$ORG['isDefaultLogo'] = 1;
+
+
+		$ORGANIZATIONS['list'][$ORG['spectrumId']] = $ORG;
 	}
 
+	if( count($ORGANIZATIONS['list']) >= $ORGANIZATIONS['count'] )
+		$crawler_notFinished = false;
 
-	$matches = array();
-	preg_match("/organization\/defaults\/logo/", $ORG['logo'], $matches);
-	if(count($matches)>0)
-		$ORG['isDefaultLogo'] = 1;
-
-
-	$ORGANIZATIONS['list'][$ORG['spectrumId']] = $ORG;
 }
 
+$ORGANIZATIONS['listCount'] = count($ORGANIZATIONS['list']);
+die('<pre>'.print_r($ORGANIZATIONS, true).'</pre>');
+$RESULT = json_encode($ORGANIZATIONS);
 
 
 
-	//die('<pre>'.print_r($ORGANIZATIONS, true).'</pre>');
-	$RESULT = json_encode($ORGANIZATIONS);
 ?>
 <textarea style="width:100%;height:100%;">
 	<?php echo($RESULT);?>
